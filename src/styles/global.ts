fe0c85fd5ea84b370640861ff488c85e83a4cf0f
GeraldoBarros <geraldo.barros@neurobots.com.br>
2021-05-16 12:18:57 -0300

    configuracoes globais de css e adicao de fontes.

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,6 +3,15 @@ import { createGlobalStyle } from 'styled-components';
 export const GlobalStyle = createGlobalStyle`
     :root {
         --background: #F0F2F5;
+        --red: #E52E4D;
+
+        -blue: #5429CC;
+        --blue-light: #6933FF;
+
+        --text-title: #363F5F;
+        --text-body: #969CB3;
+
+        --shape: #FFFFFF;
     }
     // todos os elementos
     * { 
@@ -11,8 +20,36 @@ export const GlobalStyle = createGlobalStyle`
         box-sizing: border-box;
     }
 
+    html {
+        @media (max-width: 1080px) {
+            font-size: 93.75%; 
+        }
+
+        @media (max-width: 720px) {
+            font-size: 87.5%;
+        }
+    }
+
     body {
         background: var(--background);
         -webkit-font-smoothing: antialiased; // fonts ficam mais nitidas em browsers
+    }
+
+    body, input, textarea, button {
+        font-family: 'Poppins', sans-serif; // tamanho padrão da font 400
+        font-weight: 400;
+    }
+
+    h1, h2, h3, h4, h5, h6, strong {
+        font-weight: 600;
+    }
+
+    button { 
+        cursor: pointer;
+    }
+
+    [disabled] {
+        opacity: 0.6;
+        cursor: not-allowed;
     }
 `
