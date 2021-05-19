1f43e7ae036db353fdb482bbf8489ceb9de99187
GeraldoBarros <geraldo.barros@neurobots.com.br>
2021-05-19 08:44:50 -0300

    finalizacao de estilo do NewTransactionModal, recepcao dos valores dos inputs em variaveis de estado, envio dos valores do form do modal para o banco do mirage, atualizacao da rotina do mirage para enviar informacoes cadastradas e retornar todas as informacoes do banco.

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createServer } from "miragejs";
+import { createServer, Model } from "miragejs";
 import { App } from "./App";
 
 createServer({
+  models: {
+    // banco de dados do Mirage
+    transaction: Model, // transaction e o nome da tabela
+  },
   routes() {
     // rotas que vão ser usadas na api ficticia
     this.namespace = "api"; // todas as chamadas foram feitas apartir de api
 
     this.get("/transactions", () => {
       // rota ('https://localhosts:3000/api/transactions')...
-      return [
-        // definição da informação de retorno
-        {
-          id: 1,
-          title: "Transaction 1",
-          amount: 400,
-          type: "deposit",
-          categorie: "Food",
-          createdAt: new Date(),
-        },
-      ];
+      // definição da informação de retorno
+      return this.schema.all("transaction");
+    });
+
+    this.post("/transactions", (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create("transaction", data);
     });
   },
 });
